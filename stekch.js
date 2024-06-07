d8fd7ae55427f8a8da623cf03fe87c4bb5dc95bf
AZRAELYSANTOSS <AZRAELY.SANTOS@ESCOLA.PR.GOV.BR>
2024-06-07 17:06:05 -0300

    Create stekch.js

---
+++ stekch.js
@@ -0,0 +1,19 @@
+function dividirNu
+  let numero1 = 10;
+  let numero2 = 70;
+  let resultado = numero1 * numero2;
+  console.log(resultado);
+   texto = 'o resultado da divisao é': / resultado 
+  
+  textSize(25);
+  fill("blue")
+  text(texto,5,10,350,150)meros() { 
+}   
+
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(0);
+  dividirNumeros();
